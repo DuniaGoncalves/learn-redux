ab992e41d5ea61d2033cced651e663ad7d85ac27
Dunia Goncalves <dgoncalves@everquote.com>
2018-02-02 16:06:49 -0500

    displaying and adding comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,14 +4,16 @@ import Comments from './Comments'
 
 export default class Single extends React.Component {
   render() {
+    const { postId } = this.props.params;
     // index of the post
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId];
     //get us the post
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
